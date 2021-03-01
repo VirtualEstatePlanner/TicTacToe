68c17a2560ff377be6ca3c548cbc9440fe41afaf
VirtualEstatePlanner <georgegeorgulas@gmail.com>
2021-03-01 18:12:43 -0500

    extracted celebrateVictory function

--- src/scenes/PlayGameScene.ts
+++ src/scenes/PlayGameScene.ts
@@ -2,9 +2,6 @@ import Phaser from "phaser";
 import ITicTacToeSprite from "../interfaces/ITicTacToeSprite";
 import cellClicked from "../functions/cellClicked";
 import IGameState from "../interfaces/IGameState";
-import config from "../consts/config";
-import IPlayer from "../interfaces/IPlayer";
-import IWinningLine from "../interfaces/IWinningLine";
 
 class PlayGameScene extends Phaser.Scene {
   constructor() {
@@ -24,81 +21,6 @@ class PlayGameScene extends Phaser.Scene {
 
     let cellCounter: number = 0; // count the cells as we iterate across the board
 
-    const broadcastWinner: Function = (
-      winLine: IWinningLine,
-      state: IGameState,
-      scene: PlayGameScene
-    ) => {
-      let victoryState: IGameState = { ...state };
-
-      victoryState.isGameOver = true;
-      scene.tweens.killAll();
-
-      let x = (config.scale!.width! as number) / 2;
-      let y = (config.scale!.height! as number) / 2;
-
-      let celebrationText: string;
-
-      if (gameState.winningPlayer == `X`) {
-        celebrationText = "X WINS!";
-      } else if (gameState.winningPlayer == `O`) {
-        celebrationText = "O WINS!";
-      } else {
-        celebrationText = "It's a draw!";
-      }
-
-      let label = scene.add.text(x, y, celebrationText, {
-        fontSize: "104px Arial",
-        color: "#FFFFFF",
-        backgroundColor: "#00OOFF",
-      });
-      label.setOrigin(0.5, 0.5);
-
-      label.setInteractive();
-
-      label.on(
-        "pointerdown",
-        function () {
-          scene.create();
-        },
-        this
-      );
-
-      label = scene.add.text(x, y, celebrationText, {
-        fontSize: "104px Arial",
-        color: "#O0FF00",
-      });
-      label.setOrigin(0.5, 0.5);
-
-      scene.tweens.add({
-        targets: label,
-        alpha: 0,
-        ease: "Power1",
-        duration: 1000,
-        yoyo: true,
-        repeat: -1,
-      });
-
-      if (gameState.winningPlayer != `draw`) {
-        // animate winning line of victorious player
-        for (
-          let winningCell: number = 0;
-          winningCell < winLine.length;
-          winningCell++
-        ) {
-          let sprite = victoryState.boardState[winningCell].texture;
-
-          scene.tweens.add({
-            targets: sprite,
-            angle: 360,
-            ease: "None",
-            duration: 1000,
-            repeat: -1,
-          });
-        }
-      }
-    };
-
     for (let row: number = 0; row < 3; row++) {
       let y: number = halfSquareSize + squareSize * row + row * 10; // draw row
       for (let column: number = 0; column < 3; column++) {
@@ -110,7 +32,7 @@ class PlayGameScene extends Phaser.Scene {
           `square`
         ) as ITicTacToeSprite; // generate cell
         newCell.cellKey = cellCounter++; // increment counter for that cell
-        newCell.texture = new Phaser.Textures.Texture(this.textures, `xo`);
+        newCell.setTexture(`square`);
         newCell.belongsTo = ``; // make cell empty
         newCell.setInteractive(); // make cell clickable
 
@@ -139,7 +61,7 @@ class PlayGameScene extends Phaser.Scene {
           gameState = { ...moveResultState };
         };
 
-        const shouldCellBeClickable: Function = (): void => {
+        const moveOrRestart: Function = (): void => {
           if (gameState.isGameOver) {
             this.create(); // restart game
           } else {
@@ -147,7 +69,7 @@ class PlayGameScene extends Phaser.Scene {
           }
         };
 
-        newCell.on(`pointerdown`, shouldCellBeClickable); // add trigger for click function
+        newCell.on(`pointerdown`, moveOrRestart); // add trigger for click function
 
         gameState.boardState.push(newCell);
       }
