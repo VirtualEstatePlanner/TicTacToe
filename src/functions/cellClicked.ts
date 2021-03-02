18cce7c9922514c41578d9dfac0a69495c18d930
VirtualEstatePlanner <georgegeorgulas@gmail.com>
2021-03-02 10:26:49 -0500

    now at INVALID_VALUE: texImage2D: no image

--- src/functions/cellClicked.ts
+++ src/functions/cellClicked.ts
@@ -1,19 +1,39 @@
 import IGameState from "../interfaces/IGameState";
 import ITicTacToeSprite from "../interfaces/ITicTacToeSprite";
+import imageCreator from "./imageCreator";
 import updateTurn from "./updateTurn";
+import changeSprite from "./changeSprite";
+import config from "../constants/config";
+import ox from "../../assets/ox.png";
 
 const cellClicked: Function = (
-  state: IGameState,
-  cellKey: number
+  cellKey: number,
+  scene: Phaser.Scene,
+  state: IGameState
 ): IGameState => {
   var clickedCheckState: IGameState = { ...state };
 
   if (clickedCheckState.boardState[cellKey].belongsTo == ``) {
     let newCell: ITicTacToeSprite = clickedCheckState.boardState[cellKey];
+    let textureFrame: string = ``;
+    if (clickedCheckState.currentPlayer == `X`) {
+      textureFrame = `1`;
+    }
+    if (clickedCheckState.currentPlayer == `O`) {
+      textureFrame = `0`;
+    }
     newCell.belongsTo = clickedCheckState.currentPlayer;
+    newCell.texture = new Phaser.Textures.Texture(
+      scene.textures,
+      textureFrame,
+      imageCreator(ox),
+      config.scale!.width! as number,
+      config.scale!.height! as number
+    );
     let newBoard: ITicTacToeSprite[] = clickedCheckState.boardState;
     newBoard[cellKey] = newCell;
-    const postClickState: IGameState = updateTurn(clickedCheckState);
+    let postClickState: IGameState = updateTurn(clickedCheckState);
+    changeSprite(postClickState.boardState[cellKey]);
     return postClickState;
   } else {
     return state;
--- src/functions/changeSprite.ts
+++ src/functions/changeSprite.ts
@@ -1,7 +1,8 @@
-import { GameObjects } from "phaser";
-import config from "../consts/config";
+import config from "../constants/config";
 import IGameState from "../interfaces/IGameState";
 import ITicTacToeSprite from "../interfaces/ITicTacToeSprite";
+import * as ox from "../../assets/ox.png";
+import imageCreator from "./imageCreator";
 
 const changeSprite: Function = (
   cell: ITicTacToeSprite,
@@ -10,19 +11,21 @@ const changeSprite: Function = (
 ) => {
   if (cell.belongsTo == ``) {
     if (state.currentPlayer == `X`) {
-      cell.texture = scene.add.sprite(
+      cell.texture = new Phaser.Textures.Texture(
+        scene.textures,
+        `1`,
+        imageCreator(ox) as HTMLImageElement,
         config.scale!.width! as number,
-        config.scale!.height! as number,
-        `xo`,
-        1
+        config.scale!.height! as number
       );
       cell.belongsTo = `X`;
     } else {
-      cell.texture = scene.add.sprite(
+      cell.texture = new Phaser.Textures.Texture(
+        scene.textures,
+        `0`,
+        (ox as unknown) as HTMLImageElement,
         config.scale!.width! as number,
-        config.scale!.height! as number,
-        `xo`,
-        0
+        config.scale!.height! as number
       );
       cell.belongsTo = `O`;
     }
